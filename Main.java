eb814f8cf70e611820386fba3ce17972bd679915
徳尾壮大 <tokumshr1998@gmail.com>
2021-06-24 20:05:35 +0900

    1人用モードを追加

--- Main.java
+++ Main.java
@@ -1,31 +1,69 @@
 import static java.lang.System.out;
+import java.util.*;
 public class Main{
     public static void main(String[] args){
-        out.println("ルール説明");
-	    out.println("【EAT=数字と位置が合っている】");
-	    out.println("【BITE=数字のみ合っている(位置は違う)】");
-	    out.println("先に4EATを達成した方の勝ちです");
-        out.println("アイテムを使う場合は「アイテム」または「item」と入力してください");
-
         try{
-            Player p1 = new Player(1);
-            Player p2 = new Player(2);
-            p1.set();
-            p2.set();
-            int eat;
-            Player player;
-            do{
-                eat = p1.attack(p2.getAnsNumber());
-                player = p1;
-                p1 = p2;
-                p2 = player;
-            }while(eat != 4);
-            out.println("プレイヤー" + p2.getPlayerNumber() + "の勝利です");
-            Thread.sleep(1000);
+            out.println("ルール説明");
+            out.println("【EAT=数字と位置が合っている】");
+            out.println("【BITE=数字のみ合っている(位置は違う)】");
+            out.println("先に4EATを達成した方の勝ちです");
+            out.println("アイテムを使う場合は「アイテム」または「item」と入力してください");
+            out.println("\nプレイヤー数を決めてください");
+            out.println("①　→　1 player\n②　→　2 players");
+            int number = 0;
+            while(true){
+                try{
+                    number = new Scanner(System.in).nextInt();
+                    if(number > 2 || number < 1){
+                        throw new InputMismatchException();
+                    }
+                }catch(InputMismatchException ime){
+                    out.println("1か2を入力してください");
+                    continue;
+                }
+                break;
+            }
+            if(number == 1){
+                player();
+            }else{
+                players();
+            }
         }catch(Exception e){
             out.println("想定外のエラーです");
             e.getMessage();
             e.printStackTrace();
         }
+    }
+
+    public static void player() throws Exception {
+        Player p1 = new Player(1);
+        Player enemy = new Player(2);
+        enemy.setRandom();
+        p1.getItem().setDefense(false);
+        int count = 0;
+        int eat;
+        do{
+            eat = p1.attack(enemy.getAnsNumber());
+            count++;
+        }while(eat != 4);
+        out.println(count + "回目で正解しました");
+        Thread.sleep(1000);
+    }
+
+    public static void players() throws Exception {
+        Player p1 = new Player(1);
+        Player p2 = new Player(2);
+        p1.set();
+        p2.set();
+        int eat;
+        Player player;
+        do{
+            eat = p1.attack(p2.getAnsNumber());
+            player = p1;
+            p1 = p2;
+            p2 = player;
+        }while(eat != 4);
+        out.println(p2.getName() + "の勝利です");
+        Thread.sleep(1000);
     }
 }
--- Player.java
+++ Player.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 import static java.lang.System.out;
 public class Player{
-    private int playerNumber;
+    private String name;
     private boolean ok;
     private char[] input;
     private int[] ansNumber;
@@ -12,17 +12,29 @@ public class Player{
 
     public void set() throws Exception{
         Console cons = System.console();
-        out.println("【プレイヤー" + this.getPlayerNumber() + "】4桁の数字を設定してください");
+        out.println("【" + this.getName() + "】4桁の数字を設定してください");
         while(!this.isOk()){
             this.setInput(cons.readPassword("→"));
             this.setOk(CheckNumber.check(this.input, this.ansNumber));
         }
     }
 
+    public void setRandom() throws Exception{
+        List<Integer> intList = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            intList.add(i);
+        }
+	    for(int i = 0; i < this.getAnsNumber().length; i++){
+            int random = new java.util.Random().nextInt(intList.size());
+	        this.getAnsNumber()[i] = intList.get(random);
+            intList.remove(random);
+	    }
+    }
+
     public int attack(int[] enemyNumber) throws Exception{
         this.setOk(false);
         while(!this.isOk()){
-            out.println("プレイヤー" + this.getPlayerNumber() + "の攻撃ターン");
+            out.println(this.getName() + "の攻撃ターン");
             String atk = new Scanner(System.in).nextLine();
             if(atk.equals("アイテム")||atk.equals("item")){
                 this.useItem(enemyNumber);
@@ -78,14 +90,14 @@ public class Player{
         this.setItemUsable(!this.isItemUsable()[0]);
     }
 
-    public int getPlayerNumber(){return this.playerNumber;}
+    public String getName(){return this.name;}
     public boolean isOk(){return this.ok;}
     public char[] getInput(){return this.input;}
     public int[] getAnsNumber(){return this.ansNumber;}
     public int[] getAttackNumber(){return this.attackNumber;}
     public Item getItem(){return this.item;}
     public boolean[] isItemUsable(){return this.itemUsable;}
-    public void setPlayerNumber(int pn){this.playerNumber = pn;}
+    public void setName(String name){this.name = name;}
     public void setOk(boolean ok){this.ok = ok;}
     public void setInput(char[] input){this.input = input;}
     public void setAnsNumber(int[] ansNumber){this.ansNumber = ansNumber;}
@@ -94,7 +106,7 @@ public class Player{
     public void setItemUsable(boolean iu){this.itemUsable[0] = iu;}
 
     public Player(int p){
-        this.setPlayerNumber(p);
+        this.setName("プレイヤー" + p);
         this.setAnsNumber(new int[4]);
         this.setAttackNumber(new int[4]);
         this.setOk(false);
